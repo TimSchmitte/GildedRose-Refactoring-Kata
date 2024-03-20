ee2a83936e75ab9037fc209e71595594f00879a3
tims <tim.schmitte@realdolmen.com>
2024-03-20 15:06:00 +0100

    Do some exploratory refactoring to make sense of the code

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -1,6 +1,7 @@
 package com.gildedrose;
 
 class GildedRose {
+    public static final int MAX_QUALITY = 50;
     Item[] items;
 
     public GildedRose(Item[] items) {
@@ -9,54 +10,90 @@ class GildedRose {
 
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality = items[i].quality - 1;
-                    }
-                }
-            } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
+            applyQualityAdaptations(items[i]);
 
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
+            applyAging(items[i]);
 
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-                    }
+            if (isExpired(items[i])) {
+                applyExtraAdaptationsToQualityForExpiredItem(items[i]);
+            }
+        }
+    }
+
+    private boolean isExpired(Item item) {
+        return item.sellIn < 0;
+    }
+
+    private void applyQualityAdaptations(Item item) {
+        if (!itemIs(item, "Aged Brie")
+                && !itemIs(item, "Backstage passes to a TAFKAL80ETC concert")) {
+            if (item.quality > 0) {
+                if (!itemIs(item, "Sulfuras, Hand of Ragnaros")) {
+                    decrementItemQuality(item);
                 }
             }
+        } else {
+            if (item.quality < MAX_QUALITY) {
+                incrementItemQuality(item);
 
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                items[i].sellIn = items[i].sellIn - 1;
-            }
-
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                items[i].quality = items[i].quality - 1;
-                            }
-                        }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality;
-                    }
-                } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
-                    }
+                if (itemIs(item, "Backstage passes to a TAFKAL80ETC concert")) {
+                    additionalIncrementQualityForBackstagePass(item);
                 }
             }
         }
+    }
+
+    private void applyAging(Item item) {
+        if (!itemIs(item, "Sulfuras, Hand of Ragnaros")) {
+            item.sellIn = item.sellIn - 1;
+        }
+    }
+
+    private void applyExtraAdaptationsToQualityForExpiredItem(Item item) {
+        if (!itemIs(item, "Aged Brie")) {
+            if (itemIs(item, "Backstage passes to a TAFKAL80ETC concert")) {
+                setBackStagePassValueTo0(item);
+            } else {
+                if (item.quality > 0) {
+                    if (!itemIs(item, "Sulfuras, Hand of Ragnaros")) {
+                        decrementItemQuality(item);
+                    }
+                }
+            }
+        } else {
+            if (item.quality < MAX_QUALITY) {
+                incrementItemQuality(item);
+            }
+        }
+    }
+
+    private void decrementItemQuality(Item item) {
+        item.quality = item.quality - 1;
+    }
+
+    private void setBackStagePassValueTo0(Item item) {
+        item.quality = 0;
+    }
+
+    private void additionalIncrementQualityForBackstagePass(Item item) {
+        if (item.sellIn < 11) {
+            if (item.quality < MAX_QUALITY) {
+                incrementItemQuality(item);
+            }
+        }
+
+        if (item.sellIn < 6) {
+            if (item.quality < MAX_QUALITY) {
+                incrementItemQuality(item);
+            }
+        }
+    }
+
+    private void incrementItemQuality(Item item) {
+        item.quality = item.quality + 1;
+    }
+
+    private boolean itemIs(Item item, String name) {
+        return item.name.equals(name);
     }
 }
